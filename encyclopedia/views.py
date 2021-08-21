923a4777355237d474d7a67fe4a3fea2bc4f8d4c
Luana <luana@localhost.localdomain>
2021-08-21 18:28:02 +0300

    first commit

---
+++ encyclopedia/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django import forms
+
+from . import util
+
+
+class NewForm(forms.Form):
+    title = forms.CharField(label="Title")
+    text = forms.CharField(label="Text")
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def page(request):
+    return render(request, "encyclopedia/page.html")
+
+def add(request):
+    if request.method == "POST":
+        form = NewForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            text = form.cleaned_data["text"]
+            save_entry(title, text)
+        else:
+            return render(request, "encyclopedia/add.html", {
+                "form":form
+            })
+
+    return render(request, "encyclopedia/add.html", {
+        "form" : NewForm()
+    })
+
