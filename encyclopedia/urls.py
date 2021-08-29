63bba14d182e48c0d902dacc506e6ad0d41c89e6
Luana <luana@localhost.localdomain>
2021-08-29 03:11:49 +0300

    some changes

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("add/", views.add, name="add"),
-    path("<str:title>", views.page, name="page"),
+    path("wiki/<title>", views.page, name="page"),
+    path("random", views.random, name="random"),
+    path('search/', views.search, name='search_results'),
 ]
 
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,7 +1,11 @@
 from . import util
-
-from django.shortcuts import render
+from django.views import generic
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView, ListView
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse 
 from django import forms
+from random import randint
 
 
 class NewForm(forms.Form):
@@ -9,30 +13,52 @@ class NewForm(forms.Form):
     text = forms.CharField(label="Text")
 
 
+class SearchResultsView(ListView):
+    model = util.list_entries()
+    template_name = 'search_results.html'
+    def get_queryset(self): # new
+        query = self.request.GET.get('q')
+        object_list = model.objects.filter(
+            Q(name__icontains=query) | Q(state__icontains=query)
+        )
+        return object_list
+
+
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def search(request):
+    q = request.GET.get('q').strip()
+    if q.capitalize() in util.list_entries():
+        return redirect("page", title=q)
+    return render(request, "encyclopedia/search_results.html", {"entries": util.search(q), "q": q})
+
+    # return render(request, "encyclopedia/search_results.html", {
+    #     "entries": SearchResultsView()
+    # })
+
 def page(request, title):
     try:
-        mdpage = util.get_entry(title)
-        htmlpage = md.convert(mdpage)
-        return render(request, "encyclopedia/entry.html", {
+        mdpage = util.get_entry(title.strip())
+        # htmlpage = md.convert(mdpage)
+        return render(request, "encyclopedia/page.html", {
             "title": title,
-            "content": htmlpage
+            "content": mdpage
         })
     except:
-        return render(request, "encyclopedia/error.html", {
-            "error": "Page not found!",
-            "title": "Error!"
+        return render(request, "encyclopedia/page.html", {
+             "content": "Page not found!",
+             "title": "Error!"
         })
 
 def add(request):
     if request.method == "POST":
         form = NewForm(request.POST)
         if form.is_valid():
-            title = form.cleaned_data["title"]
+            title = form.cleaned_data["title"].lower()
+            title = title.capitalize()
             text = form.cleaned_data["text"]
             util.save_entry(title, text)
         else:
@@ -44,3 +70,10 @@ def add(request):
         "form" : NewForm()
     })
 
+
+def random(request):
+    entries = util.list_entries()
+
+    juice = entries[randint(0, len(entries)-1)]
+
+    return redirect("page", title=juice.capitalize())
